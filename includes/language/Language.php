6e2b164eb3da78aac72274914a520852fc0240f9
WaitSpring <me@waitspring.com>
2024-01-04 22:00:21 +0800

    Squashed commit of the following:
    commit 07fceb885e9a5092c5608862338436f99aeecffa
    Author: Zhang Boyu <me@zhangboyu.net>
    Date:   Mon Oct 31 03:41:00 2022 +0800
    
        Remove GPS-related codes
    
        * see: <https://git.qiuwen.wiki/Qiuwen/mediawiki-qwpatchset/commit/6a03ec00ca0a200936465daf545b1e1b5d93f1b7>
    
        Signed-off-by: Zhang Boyu <me@zhangboyu.net>
    
    commit ae3f583e2c06c450efc0f51b29696bb3f473203b
    Author: WaitSpring <me@waitspring.com>
    Date:   Thu Aug 24 21:55:32 2023 +0800
    
        Remove Minguo calendar-related codes
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 22dcdfce5ce2d166700679284a7a9224390607da
    Author: WaitSpring <me@waitspring.com>
    Date:   Thu Aug 24 21:55:59 2023 +0800
    
        update languages and its names
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit a484d954487c183f07171e82b1c176d28498277b
    Author: WaitSpring <me@waitspring.com>
    Date:   Thu Aug 24 22:59:44 2023 +0800
    
        update .gitignore
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit f4ec12ea8dce3a3c5ee270892bfba54b53de530b
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 00:31:54 2023 +0800
    
        remove "generate pages for language variants" from generateSitemap.php
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 485f529b58bcd7a710fd363edd0a5ae7c7c3c642
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 00:32:44 2023 +0800
    
        update release notes
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 9cac59f9d3d43452d4395c9a5adedef8d7208ac6
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 20:25:12 2023 +0800
    
        add feature for generating HTML sitemaps
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit b30592d9733dcbf1bc957e7ca78d02b8edb3f9fd
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 20:31:35 2023 +0800
    
        fix HTML sitemaps: p => ul
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit c7ff8d37cae8d79ff69b2517e0ca1d87e46f5cfa
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 20:35:50 2023 +0800
    
        fix HTML sitemaps: Too few arguments to function GenerateSitemap::openFile()
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 3bbee1d0f5779a620ea7dde99f42c418995a4fc3
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 20:53:43 2023 +0800
    
        fix HTML sitemaps: CSS rules
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 1aea36afa20e11ea3247da7953617e91f49b76df
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 20:54:45 2023 +0800
    
        fix HTML sitemaps: Header code style
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 074580b2e4cdf392cea80fddbfb1b4aec6cb8352
    Author: WaitSpring <me@waitspring.com>
    Date:   Fri Aug 25 21:14:06 2023 +0800
    
        fix HTML sitemaps: HTML code error
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit 58917e2d3992714cbe036f53b3eda6a6319335a5
    Author: WaitSpring <me@waitspring.com>
    Date:   Thu Aug 31 00:22:41 2023 +0800
    
        Update .mailmap
    
        Signed-off-by: WaitSpring <me@waitspring.com>
    
    commit f5d97ebd76089e2ec25233e59ac486e4d3d531cc
    Author: WaitSpring <me@waitspring.com>
    Date:   Thu Aug 31 00:26:55 2023 +0800
    
        Update CREDITS
    
        Signed-off-by: WaitSpring <me@waitspring.com>

--- includes/language/Language.php
+++ includes/language/Language.php
@@ -821,9 +821,6 @@ class Language implements Bcp47Code {
 	 *
 	 *    xkY  Y (full year) in Thai solar calendar. Months and days are
 	 *                       identical to the Gregorian calendar
-	 *    xoY  Y (full year) in Minguo calendar or Juche year.
-	 *                       Months and days are identical to the
-	 *                       Gregorian calendar
 	 *    xtY  Y (full year) in Japanese nengo. Months and days are
 	 *                       identical to the Gregorian calendar
 	 *
@@ -864,7 +861,6 @@ class Language implements Bcp47Code {
 		$hebrew = false;
 		$hijri = false;
 		$thai = false;
-		$minguo = false;
 		$tenno = false;
 
 		$usedSecond = false;
@@ -1106,14 +1102,6 @@ class Language implements Bcp47Code {
 						$thai = self::tsToYear( $ts, 'thai' );
 					}
 					$num = $thai[0];
-					break;
-
-				case 'xoY':
-					$usedYear = true;
-					if ( !$minguo ) {
-						$minguo = self::tsToYear( $ts, 'minguo' );
-					}
-					$num = $minguo[0];
 					break;
 
 				case 'xtY':
@@ -1677,11 +1665,9 @@ class Language implements Bcp47Code {
 	}
 
 	/**
-	 * Algorithm to convert Gregorian dates to Thai solar dates,
-	 * Minguo dates or Minguo dates.
+	 * Algorithm to convert Gregorian dates to Thai solar dates.
 	 *
 	 * Link: https://en.wikipedia.org/wiki/Thai_solar_calendar
-	 *       https://en.wikipedia.org/wiki/Minguo_calendar
 	 *
 	 * @param string $ts 14-character timestamp
 	 * @param string $cName Calendar name
@@ -1705,11 +1691,6 @@ class Language implements Bcp47Code {
 				}
 				$gm = ( $gm - 3 ) % 12;
 			}
-		} elseif ( $cName === 'minguo' || $cName === 'juche' ) {
-			# Minguo dates
-			# Deduct 1911 years from the Gregorian calendar
-			# Months and days are identical
-			$gy_offset = $gy - 1911;
 		} else {
 			$gy_offset = $gy;
 		}
@@ -1968,7 +1949,6 @@ class Language implements Bcp47Code {
 			'i' => 'persian', // Iranian
 			'm' => 'islamic', // Hijri
 			'k' => 'buddhist', // Thai
-			'o' => 'roc', // Minguo
 			't' => 'japanese' // Tenno
 		];
 
